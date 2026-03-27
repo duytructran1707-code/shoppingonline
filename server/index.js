6a2e94775969cadc85e9fd71cc923c588217ada8
duytructran1707-code <duytructran1707@gmail.com>
2026-03-27 12:35:18 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,32 +1,34 @@
-// CLI : npm install express body-parser --save
 const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-
-// middlewares
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-// apis
+
+// API
 app.get('/hello', (req, res) => {
     res.json({ message: 'Hello from server!' });
 });
+
 app.use('/api/admin', require('./api/admin.js'));
-app.use('/api/customer', require('./api/customer.js')); 
+app.use('/api/customer', require('./api/customer.js'));
 
-// deployment
+// ADMIN FRONTEND
 app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+app.get('/admin/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'));
 });
 
-//start server
+// CUSTOMER FRONTEND
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
+
+// START SERVER
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
