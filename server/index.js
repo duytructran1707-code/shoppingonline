5b78b38d855868781e56ba87fd9951c7165d0828
duytructran1707-code <duytructran1707@gmail.com>
2026-03-27 11:09:04 +0700

    server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+// CLI : npm install express body-parser --save
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+
+// middlewares
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+// apis
+app.get('/hello', (req, res) => {
+    res.json({ message: 'Hello from server!' });
+});
+app.use('/api/admin', require('./api/admin.js'));
+app.use('/api/customer', require('./api/customer.js')); 
+
+// deployment
+app.use('/admin', express.static(path.resolve(__dirname, '../client-admin/build')));
+app.get('admin/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-admin/build', 'index.html'))
+});
+app.use('/', express.static(path.resolve(__dirname, '../client-customer/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client-customer/build', 'index.html'));
+});
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,15 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.8bsv43m.mongodb.net',
+    DB_USER: 'root',
+    DB_PASS: 'A12345',
+    DB_DATABASE: 'shoppingonline',
+
+    EMAIL_USER: 'duytructran1707@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'mtsntqnuvjyvbehz',
+
+    JWT_SECRET: 'abc',
+    JWT_EXPIRES: '99999999999999999999999' // in milliseconds
+  };
+
+module.exports = MyConstants;
+  
